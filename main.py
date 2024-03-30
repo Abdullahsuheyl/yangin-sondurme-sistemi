524c78aea8ce09fb8fd4102bd4f1cc51b6d39f6d
Abdullah Süheyl <89685930+Abdullahsuheyl@users.noreply.github.com>
2024-03-30 14:32:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ upper_red2 = np.array([180, 255, 255])
 
 # Kamera ortasındaki çemberin parametreleri
 radius = 75
-height, width = 480, 640  # Varsayılan kamera çözünürlüğü
+height, width = 480, 640  
 center_x = int(width / 2)
 center_y = int(height / 2)
 center_video = (center_x, center_y)
 
-# Eşleşme için bekleme süresi
+
 match_time = 5
 start_time_blue = None
 start_time_red = None
@@ -35,16 +35,15 @@ if camera.isOpened():
         if not ret:
             break
         
-        # Video Orta Noktası Hesaplama
+       
         cv.circle(frame, center_video, radius, (0, 255, 0), 2)
 
-        # HSV renk uzayına dönüştür
+        
         hsv=cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
         # Mavi nesneleri tespit et
         if not blue_detected:
             blue_mask = cv.inRange(hsv, lower_blue, upper_blue)
-            # Mavi maskesi üzerinde morfolojik açma işlemi uygula
             blue_mask = cv.morphologyEx(blue_mask, cv.MORPH_OPEN, kernel)
             blue_contours, _ = cv.findContours(blue_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
             
@@ -56,13 +55,13 @@ if camera.isOpened():
                     cx = int(M["m10"] / M["m00"])
                     cy = int(M["m01"] / M["m00"])
                     
-                    # Mavi nesnelerin alanını hesapla
+
                     area = cv.contourArea(contour)
                     
                     
-                    # Alanı kontrol et ve belirli bir boyuttan büyükse işlem yap
+
                     if area > 800:
-                        # Merkezi çemberin merkezine göre kontrol et
+
                         if (cx - center_x)**2 + (cy - center_y)**2 < radius**2:
                             cv.drawContours(frame, [contour], -1, (255, 0, 0), 2)
                             if start_time_blue is None:
@@ -76,7 +75,7 @@ if camera.isOpened():
         if not red_detected:
             red_mask1 = cv.inRange(hsv, lower_red1, upper_red1)
             red_mask2 = cv.inRange(hsv, lower_red2, upper_red2)
-            # Kırmızı maskesi üzerinde morfolojik açma işlemi uygula
+
             red_mask = cv.bitwise_or(red_mask1, red_mask2)
             red_mask = cv.morphologyEx(red_mask, cv.MORPH_OPEN, kernel)
             red_contours, _ = cv.findContours(red_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
@@ -87,12 +86,10 @@ if camera.isOpened():
                     cx = int(M["m10"] / M["m00"])
                     cy = int(M["m01"] / M["m00"])
                     
-                    # Kırmızı nesnelerin alanını hesapla
                     area = cv.contourArea(contour)
                     
-                    # Alanı kontrol et ve belirli bir boyuttan büyükse işlem yap
-                    if area > 100:
-                        # Merkezi çemberin merkezine göre kontrol et
+
+                    if area > 100:             
                         if (cx - center_x)**2 + (cy - center_y)**2 < radius**2:
                             
                             cv.drawContours(frame, [contour], -1, (0, 0, 255), 2)
